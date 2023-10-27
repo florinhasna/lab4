32d2f82d7d7a3d7f43b547af48b91be4eeefc066
Florin Hasna <florinhasna@outlook.com>
2023-10-27 10:28:07 +0100

    Added check condition to see if input is a digit or punctuation mark.

--- chartype.cpp
+++ chartype.cpp
@@ -2,6 +2,11 @@
 #include <string>
 using namespace std;
 
+char isPunctuation(char input){
+  string punctuationMarks = ".,;:?!\"/()[]_@";
+  return punctuationMarks.find(input);
+}
+
 char isVowel(char input){
   string vowels = "aeiouAEIOU";
   return vowels.find(input);
@@ -19,4 +24,8 @@ int main(){
       else
 	cout << input << " is a consonant.\n";
   }
+  else if (input >= '0' && input <= '9')
+    cout << input << " is a digit. \n";
+  else if (isPunctuation(input) != string::npos)
+    cout << input << " is a punctuation mark.\n";
 }
