d1d5faf07aba0f85252e514a4d815ae421ccc0aa
Florin Hasna <florinhasna@outlook.com>
2023-10-27 10:16:04 +0100

    Checking if the input is a vowel

--- chartype.cpp
+++ chartype.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+char isVowel(char input){
+  string vowels = "aeiouAEIOU";
+  return vowels.find(input);
+}
 
 int main(){
   char input;
   cout << "Enter a character: ";
   cin >> input;
+
+  if (input >= 'a' && input <= 'z' || input >= 'A' && input <= 'Z'){
+    string vowels = "aeiouAEIOU";
+    if (isVowel(input) != string::npos)
+	cout << input << " is a vowel.\n";
+      else
+	cout << input << " is a consonant.\n";
+  }
 }
